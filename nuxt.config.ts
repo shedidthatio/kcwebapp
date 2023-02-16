b20ba9d3f6e8a2b43dc2c9328806d3c16591d03b
Kat <shedidthatio22@gmail.com>
2023-02-16 13:47:03 -0500

    head, meta data, and purecss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+  app: {
+    head: {
+      htmlAttrs: { lang: "en" },
+      title: "Vue and Nuxt portfolio",
+      meta: [
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        { charset: "utf-8" },
+        { name: "description", content: "A portfolio made with Vue and Nuxt" },
+      ],
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://cdn.jsdelivr.net/npm/purecss@3.0.0/build/pure-min.css",
+          integrity:
+            "sha384-X38yfunGUhNzHpBaEBsWLO+A0HDYOQi8ufWDkZ0k9e0eXz/tH3II7uKZ9msv++Ls",
+          crossorigin: "anonymous",
+        },
+      ],
+    },
+  },
+});
