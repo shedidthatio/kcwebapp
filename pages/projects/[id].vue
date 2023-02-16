27c446574209664ef68bede89c26bab43882580c
Kat <shedidthatio22@gmail.com>
2023-02-16 12:56:36 -0500

    pages organization

--- pages/projects/[id].vue
+++ pages/projects/[id].vue
@@ -1,8 +1,6 @@
 <template>
     <div>
-        <p>Project details for {{ id }}</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium nesciunt voluptatem vitae laudantium
-            veritatis itaque doloremque esse ad officiis incidunt.</p>
+
 </div>
 </template>
 
--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,6 +1,10 @@
 <template>
     <div>
-        <h1>Projects page</h1>
+        <h1>All Projects page</h1>
+        <h2>Projects that have helped real users with real issues.</h2>
+        <p>This is the home of projects I've contributed heavily towards. There's 4 total that
+            I'm showcasing. However, I've worked on so much more over the course of my career.
+            Additional projects can be found at the bottom after the showcased projects. </p>
 </div>
 </template>
 
