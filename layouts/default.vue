b8da084f2d5497f4429b092a3ed13ab3b04d45d1
Kat <shedidthatio22@gmail.com>
2023-02-16 12:43:23 -0500

    layouts dir and default page layout setup

---
+++ layouts/default.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <header>
+            <nav>
+                <ul>
+                    <li>
+                        <NuxtLink to='/'>Brand</NuxtLink>
+                        <NuxtLink to='/projects'>Projects</NuxtLink>
+                        <NuxtLink to='/about'>About</NuxtLink>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+
+        <!-- Output the page content -->
+        <div>
+            <slot />
+        </div>
+</div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped></style>
