bb7f59b3b7b68d258048ada8eee3798bfbc101b8
Kat <shedidthatio22@gmail.com>
2023-02-16 12:01:02 -0500

    routing setup useRoute()

---
+++ pages/projects/[id].vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <p>Project details for {{ id }}</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium nesciunt voluptatem vitae laudantium
+            veritatis itaque doloremque esse ad officiis incidunt.</p>
+</div>
+</template>
+
+<script setup lang="ts">
+const { id } = useRoute().params
+</script>
+
+<style scoped></style>
