1175296b610765af305020413cbfcbb681f541ee
Kat <shedidthatio22@gmail.com>
2023-02-21 14:01:05 -0500

    setup Sass for styling

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,4 +20,14 @@ export default defineNuxtConfig({
       ],
     },
   },
+  css: ["@/assets/styles/main.scss"],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@import "@/assets/styles/_variables.scss";',
+        },
+      },
+    },
+  },
 });
