c031e85e6e4c30cf318a76d8c8306cadd56a0aad
Kat <shedidthatio22@gmail.com>
2023-03-01 10:15:23 -0500

    added Bootstrap and FontAwesome

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,9 +12,27 @@ export default defineNuxtConfig({
       link: [
         {
           rel: "stylesheet",
-          href: "https://cdn.jsdelivr.net/npm/purecss@3.0.0/build/pure-min.css",
+          href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css",
+        },
+        {
+          rel: "stylesheet",
+          href: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css",
           integrity:
-            "sha384-X38yfunGUhNzHpBaEBsWLO+A0HDYOQi8ufWDkZ0k9e0eXz/tH3II7uKZ9msv++Ls",
+            "sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD",
+          crossorigin: "anonymous",
+        },
+      ],
+      script: [
+        {
+          src: "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js",
+          integrity:
+            "sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3",
+          crossorigin: "anonymous",
+        },
+        {
+          src: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js",
+          integrity:
+            "sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD",
           crossorigin: "anonymous",
         },
       ],
