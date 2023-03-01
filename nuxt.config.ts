0108b11b2386fc07deb9fbe00e1ba99e6c7e37a0
Kat <shedidthatio22@gmail.com>
2023-03-01 18:40:53 -0500

    removed aos cdn

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,10 +21,6 @@ export default defineNuxtConfig({
             "sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD",
           crossorigin: "anonymous",
         },
-        {
-          rel: "stylesheet",
-          href: "https://unpkg.com/aos@2.3.1/dist/aos.css",
-        },
       ],
       script: [
         {
@@ -38,9 +34,6 @@ export default defineNuxtConfig({
           integrity:
             "sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD",
           crossorigin: "anonymous",
-        },
-        {
-          src: "https://unpkg.com/aos@2.3.1/dist/aos.js",
         },
       ],
     },
