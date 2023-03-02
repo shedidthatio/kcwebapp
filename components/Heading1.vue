622d8175e36aa30c55e36c5c602ca22dfd68dff2
Kat <shedidthatio22@gmail.com>
2023-03-02 05:18:32 -0500

    created H1 component

---
+++ components/Heading1.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1>Off Guards Photography</h1>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang='scss'>
+h1 {
+    font-size: 4.6rem;
+    border-radius: 100px;
+    background-color: transparent;
+    display: inline-block;
+    max-width: max-content;
+    padding-top: 2%;
+}
+
+@media (max-width: 798px) {
+    h1 {
+        font-size: 2.8rem;
+        padding-top: 0;
+    }
+}
+</style>
